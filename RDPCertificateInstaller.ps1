86b5c7c1b245bb6715c20b3d7be7e3489e3bced3
Veltys <veltys@veltys.es>
2022-08-19 18:10:54 +0200

    ADD: Certificate setter

--- RDPCertificateInstaller.ps1
+++ RDPCertificateInstaller.ps1
@@ -3,7 +3,7 @@
 # Description   : Script to automatically install as RDP certificate a given one by its domain name
 # Author        : Veltys
 # Date          : 2022-08-19
-# Version       : 0.1.0
+# Version       : 1.0.0
 # Usage         : powershell RDPCertificateInstaller.ps1
 # Notes         : Must be run as superuser
 
@@ -21,3 +21,5 @@ foreach($cert in $certs) {
 }
 
 echo $thumbprint
+
+Set-WmiInstance -Path (Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices) -argument @{SSLCertificateSHA1Hash=$thumbprint}
