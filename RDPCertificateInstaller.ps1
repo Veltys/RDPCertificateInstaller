24f9bf878642e0cdc6a988e0c651bfaca700f425
Veltys <veltys@veltys.es>
2022-08-19 18:09:16 +0200

    ADD: Domain name filtering

--- RDPCertificateInstaller.ps1
+++ RDPCertificateInstaller.ps1
@@ -3,8 +3,21 @@
 # Description   : Script to automatically install as RDP certificate a given one by its domain name
 # Author        : Veltys
 # Date          : 2022-08-19
-# Version       : 0.0.1
+# Version       : 0.1.0
 # Usage         : powershell RDPCertificateInstaller.ps1
 # Notes         : Must be run as superuser
 
-Get-ChildItem "Cert:\LocalMachine\My"
+
+$domain = ""																	# Certificate domain name
+
+$certs = Get-ChildItem "Cert:\LocalMachine\My"
+
+foreach($cert in $certs) {
+    if($cert.Subject -eq "CN=$domain") {
+        $thumbprint = $cert.Thumbprint
+
+        break
+    }
+}
+
+echo $thumbprint
