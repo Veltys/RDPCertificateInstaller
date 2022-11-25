6271bdb8f7f67b3bdd98d9e67d963a1ea64e5e89
Veltys <veltys@veltys.es>
2022-11-25 13:46:01 +0100

    FIX: Code quality

--- RDPCertificateInstaller.ps1
+++ RDPCertificateInstaller.ps1
@@ -2,8 +2,8 @@
 # Title         : RDPCertificateInstaller.ps1
 # Description   : Script to automatically install as RDP certificate a given one by its domain name
 # Author        : Veltys
-# Date          : 2022-08-19
-# Version       : 1.0.0
+# Date          : 2022-11-25
+# Version       : 1.0.1
 # Usage         : powershell RDPCertificateInstaller.ps1
 # Notes         : Must be run as superuser
 
@@ -22,4 +22,4 @@ foreach($cert in $certs) {
 
 echo $thumbprint
 
-Set-WmiInstance -Path (Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices) -argument @{SSLCertificateSHA1Hash=$thumbprint}
+Set-CimInstance -CimInstance (Get-CimInstance -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices) -Property @{SSLCertificateSHA1Hash=$thumbprint}
